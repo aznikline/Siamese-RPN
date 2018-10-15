0ab471a1bb398ec23666c3c051451d20d6e1ffbf
Yuol96 <huangyc96@gmail.com>
2018-10-15 22:05:24 +0800

    change threshold, change dataset_name, change lmbda

--- config.py
+++ config.py
@@ -9,18 +9,19 @@ __C.PATH.root_dir = Path('/unsullied/sharefs/huangyucheng/data/dl-github/Siamese
 # __C.PATH.train_dir = __C.PATH.root_dir / 'data' / '64scale_train_dataset' / 'train'
 __C.PATH.experiment_dir = __C.PATH.root_dir / 'experiment'
 
+# __C.dataset_name = '64scale_train_dataset'
 __C.dataset_name = 'random_insert'
 
 __C.template_size = 127
 __C.detection_size = 255
-__C.lmbda = 1
+__C.lmbda = 0.1
 
-__C.pos_iou_thresh = 0.5
-__C.neg_iou_thresh = 0.2
+__C.pos_iou_thresh = 0.6
+__C.neg_iou_thresh = 0.3
 
 __C.TRAIN = edict()
 __C.TRAIN.momentum = 0.9
-__C.TRAIN.batch_size=1
+# __C.TRAIN.batch_size=1
 
 __C.TEST = edict()
 __C.TEST.max_validation = 3000
